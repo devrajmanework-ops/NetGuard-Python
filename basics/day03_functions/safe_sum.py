0562f71547f57f0dd60b87019cc3ddf070035e72
dev <manedevraj01@gmail.com>
2026-01-28 23:26:22 +0530

    Day 3 Task 2: safe sum with type validation

---
+++ basics/day03_functions/safe_sum.py
@@ -0,0 +1,13 @@
+data = [1, 2, "a", 3.5, None, 4]
+ 
+def safe_sum(data):
+
+    total = 0
+
+    for n in data:
+     if isinstance(n,(int,float)):
+      total = total + n
+    return total 
+
+result = safe_sum(data)
+print("sum is:",result)
