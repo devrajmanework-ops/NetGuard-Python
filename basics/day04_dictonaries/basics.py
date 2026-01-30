495a881ae3dc04a3a25a7db4b3205f9b6df6f32c
dev <manedevraj01@gmail.com>
2026-01-30 13:12:34 +0530

    Day 4: dictionary basics (create, access, update)

---
+++ basics/day04_dictonaries/basics.py
@@ -0,0 +1,10 @@
+student = {
+    "name" : "devraj",
+    "age" : 23,
+    "course" : "cyber security",
+    "email" : "abcd@gmail.com"
+    }
+print (student["name"])
+print (student["age"])
+print (student["course"])
+print (student["email"])
